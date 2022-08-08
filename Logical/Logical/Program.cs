91da72c522cc2e0d7d4d30b9a947aa37e5ce1b95
Vandanalalli06 <vandanalalli06@gmail.com>
2022-08-08 23:37:59 +0530

    Welcome to Logical Programming Problems

---
+++ Logical/Logical/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LogicalProgram
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            PerfectNumberProblem cnum = new PerfectNumberProblem();
+            cnum.PerfectNumber();
+
+        }
+    }
+}
+
+
+
+
+
